7cdb15886216faea45ca4b5b4b0d3043bfe3a92d
Sar222324 <saraaalamin04@gmail.com>
2026-08-21 17:30:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website Informasi Buku & Literasi berhasil dimuat!");
+    
+   
+    alert("Selamat datang di Website Informasi Buku & Literasi!");
+});
